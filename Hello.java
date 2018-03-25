3576b3bb41aba28287a0b26d859988373f937c72
amadou <ndiayeammadou23@gmail.com>
2018-03-25 23:13:16 +0000

    Saisir au clavier

--- Hello.java
+++ Hello.java
@@ -1,9 +1,14 @@
+import java.util.Scanner;
 public class Hello {
 	
+	Scanner sc = new Scanner(System.in);
 	public static void main(String[] args) {
 		
 		System.out.println("Hello Word !!!");
 		System.out.println("Test réussi !");
+		System.out.println("Veuillez saisir un entier");
+		int p = sc.nextInt();
+		System.out.println(p);
 	}
 	
 }
