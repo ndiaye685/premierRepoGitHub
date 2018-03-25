02f9475aadb016e28322a42786376c2f91599f5b
amadou <ndiayeammadou23@gmail.com>
2018-03-25 23:18:36 +0000

    test2

--- Hello.java
+++ Hello.java
@@ -4,11 +4,8 @@ public class Hello {
 	Scanner sc = new Scanner(System.in);
 	public static void main(String[] args) {
 		
-		System.out.println("Hello Word !!!");
-		System.out.println("Test réussi !");
-		System.out.println("Veuillez saisir un entier");
-		int p = sc.nextInt();
-		System.out.println(p);
+		System.out.println("Hello Word ....!!!");
+
 	}
 	
 }
